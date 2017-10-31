514ba8976d8e9034531ddf662c133cafd7e14adf
Mullen, Vernon <vernon.mullen@siriusxm.com>
2017-10-31 00:01:51 -0500

    working on refining null state upon search

--- public/js/mainCtrl.js
+++ public/js/mainCtrl.js
@@ -1,12 +1,14 @@
 angular.module('app').controller('mainCtrl', function($scope, mainSrv) {
   console.log('controlling');
   $scope.test = "working"
+  $scope.searched = false
 
   $scope.getAddress = function(input) {
     mainSrv.getAddress(input)
     .then(function(response) {
       $scope.yourInfo = mainSrv.yourInfo
       $scope.matchedRep = mainSrv.matchedRep
+      $scope.searched = true
     })
   }
 
